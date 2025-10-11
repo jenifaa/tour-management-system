1997ece9f909937ad2591b70c33e38042bcc78ca
jenifaa <jefrahman32@gmail.com>
2025-10-11 22:49:16 +0600

    setup done

---
+++ src/server.ts
@@ -0,0 +1,48 @@
+import { Server } from "http";
+
+import mongoose from "mongoose";
+import app from "./app";
+import { envVars } from "./app/config/env";
+let server: Server;
+
+const startServer = async () => {
+  try {
+    await mongoose.connect(envVars.DB_URL);
+    console.log("connected to db");
+    server = app.listen(envVars.PORT, () => {
+      console.log(`Server is listening at ${envVars.PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
+
+process.on("SIGTERM", () => {
+  console.log("SIGTERM detected");
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+process.on("unhandledRejection", () => {
+  console.log("UnhandledRejection detected");
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
+process.on("uncaughtException", () => {
+  console.log("uncaughtException detected");
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+  process.exit(1);
+});
